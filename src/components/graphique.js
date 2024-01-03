140904ae484bfbba992ed60e3d2cf216dfd97c76
FlorentMarien <florent62350@hotmail.fr>
2024-01-03 12:08:20 +0100

    Ajout - Rechart Graphique

--- src/components/graphique.js
+++ src/components/graphique.js
@@ -1,8 +1,18 @@
-
+import SimpleRadarChart from './graphique/simpleradarchart'
+import SimpleBarChart from './graphique/simplebarchart'
+import SimpleLineChart from './graphique/simplelinechart'
+import SimpleRadialBarChart from './graphique/simpleradialbarchart'
+import './../styles/graphique.css'
 function Graphique() {
   return (
     <section>
       <p>test3</p>
+      <div class="container-graph">
+        <SimpleBarChart></SimpleBarChart>
+        <SimpleRadarChart></SimpleRadarChart>
+        <SimpleLineChart></SimpleLineChart>
+        <SimpleRadialBarChart></SimpleRadialBarChart>
+      </div>
     </section>
   );
 }
