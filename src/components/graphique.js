fdf759448029203d5acede009e1c18bec21cc455
FlorentMarien <florent62350@hotmail.fr>
2024-01-04 12:19:20 +0100

    Amélioration - simplebarchart

--- src/components/graphique.js
+++ src/components/graphique.js
@@ -4,11 +4,48 @@ import SimpleLineChart from './graphique/simplelinechart'
 import SimpleRadialBarChart from './graphique/simpleradialbarchart'
 import './../styles/graphique.css'
 function Graphique() {
+  const data = [
+    {
+      name: '1',
+      poids: 60,
+      calories: 350,
+    },
+    {
+      name: '2',
+      poids: 80,
+      calories: 400,
+    },
+    {
+      name: '3',
+      poids: 90,
+      calories: 500,
+    },
+    {
+      name: '4',
+      poids: 80,
+      calories: 420,
+    },
+    {
+      name: '5',
+      poids: 60,
+      calories: 350,
+    },
+    {
+      name: '6',
+      poids: 60,
+      calories: 350,
+    },
+    {
+      name: '7',
+      poids: 60,
+      calories: 350,
+    },
+  ];
   return (
     <section>
       <p>test3</p>
       <div class="container-graph">
-        <SimpleBarChart></SimpleBarChart>
+        <SimpleBarChart Data={data}></SimpleBarChart>
         <SimpleRadarChart></SimpleRadarChart>
         <SimpleLineChart></SimpleLineChart>
         <SimpleRadialBarChart></SimpleRadialBarChart>
--- src/components/graphique/simplebarchart.js
+++ src/components/graphique/simplebarchart.js
@@ -1,61 +1,34 @@
-import React, { PureComponent } from 'react';
+import React, { PureComponent, useRef } from 'react';
 import { BarChart, Bar, Rectangle, XAxis, YAxis, CartesianGrid, Tooltip, Legend, ResponsiveContainer } from 'recharts';
 
-const data = [
-  {
-    name: 'Page A',
-    uv: 4000,
-    pv: 2400,
-    amt: 2400,
-  },
-  {
-    name: 'Page B',
-    uv: 3000,
-    pv: 1398,
-    amt: 2210,
-  },
-  {
-    name: 'Page C',
-    uv: 2000,
-    pv: 9800,
-    amt: 2290,
-  },
-  {
-    name: 'Page D',
-    uv: 2780,
-    pv: 3908,
-    amt: 2000,
-  },
-  {
-    name: 'Page E',
-    uv: 1890,
-    pv: 4800,
-    amt: 2181,
-  },
-  {
-    name: 'Page F',
-    uv: 2390,
-    pv: 3800,
-    amt: 2500,
-  },
-  {
-    name: 'Page G',
-    uv: 3490,
-    pv: 4300,
-    amt: 2100,
-  },
-];
+function mimaxPoid(data){
+    let min = undefined;
+    let max = undefined;
+    data.forEach(element => {
+        if(element.poid < min || min === undefined) min = element.poid;
+        if(element.poid > max || min === undefined) max = element.poid;
+    });
+    console.log(min + " " +max)
+    return [min,max]
+}
+
+let localData;
 
 export default class Example extends PureComponent {
-  static demoUrl = 'https://codesandbox.io/s/simple-bar-chart-tpz8r';
-
+  //static demoUrl = 'https://codesandbox.io/s/simple-bar-chart-tpz8r';
+  
+  constructor(Data){
+    super(Data);
+    localData = Data.Data;
+  }
+  
   render() {
     return (
       <ResponsiveContainer width="100%" height="100%">
         <BarChart
           width={500}
           height={300}
-          data={data}
+          data={localData}
           margin={{
             top: 5,
             right: 30,
@@ -65,11 +38,11 @@ export default class Example extends PureComponent {
         >
           <CartesianGrid strokeDasharray="3 3" />
           <XAxis dataKey="name" />
-          <YAxis />
+          <YAxis dataKey="poids" domain={mimaxPoid(localData)}/>
           <Tooltip />
           <Legend />
-          <Bar dataKey="pv" fill="#8884d8" activeBar={<Rectangle fill="pink" stroke="blue" />} />
-          <Bar dataKey="uv" fill="#82ca9d" activeBar={<Rectangle fill="gold" stroke="purple" />} />
+          <Bar dataKey="poids" fill="black" activeBar={<Rectangle fill="gray" stroke="orange" />} />
+          <Bar dataKey="calories" fill="red" activeBar={<Rectangle fill="orange" stroke="gray" />} />
         </BarChart>
       </ResponsiveContainer>
     );
