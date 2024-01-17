896de64aaf50fa2c04da9e875d17a71a7594caa0
FlorentMarien <florent62350@hotmail.fr>
2024-01-17 14:03:14 +0100

    Amélioration - mise en page

--- src/components/graphique.js
+++ src/components/graphique.js
@@ -5,7 +5,6 @@ import SimpleRadialBarChart from './graphique/simpleradialbarchart'
 import Nutrition from './nutrition.js'
 import './styles/graphique.css'
 import Services from './mocked/services.js'
-    let dataNutrition = {id:0};
     let data = new Services();
     let userActivity = data.getUserActivity();
     let userPerformance = data.getUserPerformances();
@@ -16,31 +15,38 @@ function Graphique() {
   
   
   return (
-    <>
-    <section>
-      <p>test3</p>
-      <div className="container-graph">
-        <div className='container-simplebarchart'>
-        <SimpleBarChart Data={userActivity}></SimpleBarChart>
-        </div>
-        <div className='container-radarchart'>
-          <SimpleRadarChart Data={userPerformance}></SimpleRadarChart>
-        </div>
-        <div className='container-linechart'>
-          <SimpleLineChart Data={userAverageSessions}></SimpleLineChart>
-        </div>
-        <div className='container-radialbarchart'>
-        <SimpleRadialBarChart  Data={userMainData}></SimpleRadialBarChart>
+    <div id="main-container">
+      <p className='text-welcome'>Bonjour <span>{'Thomas'}</span></p>
+      <p>Félicitation ! Vous avez explosé vos objectifs hier 👏</p>
+      <div>
+        <div>
+          <section className='block-graphique'>
+          
+          <div className="container-graph">
+            <div className='container-simplebarchart'>
+            <SimpleBarChart Data={userActivity}></SimpleBarChart>
+            </div>
+            <div className='container-radarchart'>
+              <SimpleRadarChart Data={userPerformance}></SimpleRadarChart>
+            </div>
+            <div className='container-linechart'>
+              <SimpleLineChart Data={userAverageSessions}></SimpleLineChart>
+            </div>
+            <div className='container-radialbarchart'>
+            <SimpleRadialBarChart  Data={userMainData}></SimpleRadialBarChart>
+            </div>
+          </div>
+        </section>
+        <section className='block-nutrition'>
+          <Nutrition data={{id:1}}></Nutrition>
+          <Nutrition data={{id:2}}></Nutrition>
+          <Nutrition data={{id:3}}></Nutrition>
+          <Nutrition data={{id:4}}></Nutrition>
+        </section>
         </div>
       </div>
-    </section>
-    <section>
-      <Nutrition data={{id:1}}></Nutrition>
-      <Nutrition data={{id:2}}></Nutrition>
-      <Nutrition data={{id:3}}></Nutrition>
-      <Nutrition data={{id:4}}></Nutrition>
-    </section>
-    </>
+    
+    </div>
   );
 }
 
