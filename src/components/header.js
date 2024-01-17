d3f8ac98f9737e80ed45d20fea9903aa6d0572d3
FlorentMarien <florent62350@hotmail.fr>
2024-01-17 11:27:03 +0100

    Ajout - header navbar

--- src/components/header.js
+++ src/components/header.js
@@ -1,9 +1,21 @@
-
+import './styles/header.css'
+import iconSportSee from './assets/icons/sportsseeicon.svg';
+import titleSportSee from './assets/icons/sportsseetitle.svg';
 function Header() {
   return (
     
       <header>
-        <nav><p>test</p></nav>
+        <nav>
+          <div>
+            <img src={iconSportSee} />
+            <img src={titleSportSee} />
+          </div>
+          <a href="#">Accueil</a>
+          <a href="#">Profil</a>
+          <a href="#">Réglages</a>
+          <a href="#">Communauté</a>
+
+        </nav>
       </header>
       
   );
